19728ba48343397aea093b5fe07070be983272e7
minecraft-sx <151476172+elyxdev@users.noreply.github.com>
2025-03-03 01:52:01 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 color a
 title Compilando Modgest
+rmdir /s /q dist
 pyinstaller --clean --onefile --name Modgest_Win --icon .\img\modrinth_icon.ico modgest.py
 rmdir /s /q build
 del *.spec
